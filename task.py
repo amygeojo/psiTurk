7441aeb1642fbf6cba49447af0501b92a35c5e6a
John McDonnell <john.v.mcdonnell@gmail.com>
2012-04-04 10:21:58 -0400

    Not well tested but task seems to work.

--- task.py
+++ task.py
@@ -34,19 +34,26 @@ def build_abstract_grid( n ):
     coords = [[x,y] for x in np.linspace(*rect[0], num=rootn) for y in np.linspace(*rect[1], num=rootn)]
     return np.column_stack([coords, [np.nan]*n, [np.nan]*n])
 
-class Task:
+class tvTask:
     def __init__(self,
                  order = "interspersed",
-                 nlab=0,
-                 nbasis=240,
-                 ntest=50,
-                 testform="bimod",
-                 axis="size",
-                 anglerotBal=0, 
-                 swapcorners=False, 
-                 swapidentity=False,
+                 nbasis = 240,
+                 nlab = 0,
+                 ntest = 50,
+                 testform = "bimod",
+                 axis = None,
+                 swapcorners = None, 
+                 swapidentity = None,
+                 angleoffset = None,
+                 lengthoffset = 30,
                  anglerange = 60,
                  lengthrange = 120):
+        
+        # These params need to not take default values:
+        assert axis
+        assert angleoffset
+        assert swapcorners
+        assert swapidentity
         
         self.order = "interspersed"
         self.nlab = 0
@@ -54,9 +61,10 @@ class Task:
         self.ntest = ntest
         self.testform = testform
         self.axis = axis
-        self.anglerotBal = anglerotBal
         self.swapcorners = swapcorners
         self.swapidentity = swapidentity
+        self.angleoffset = angleoffset
+        self.lengthoffset = lengthoffset
         self.anglerange = anglerange
         self.lengthrange = lengthrange
         
@@ -68,11 +76,15 @@ class Task:
         self.bounds = {'length': [self.lengthoffset, self.lengthrange+self.lengthoffset], 
                        'angle': [self.angleoffset, self.anglerange+self.angleoffset]}
         self.stacked = self.nlab > (self.nbasis / 7)
+        
+        trainAbstract, testAbstract = self.build_abstract_task()
+        self.train = [ self.transform_stim(stim) for stim in trainAbstract ]
+        self.test = [ self.transform_stim(stim) for stim in testAbstract ]
     
     # Check various properties of the inputs
     def checkinputs(self):
         assert self.nbasis % 28 == 0
-        assert self.testform in ["bimodal", "grid"]
+        assert self.testform in ["bimodal", "grid", "mixed"] 
         assert self.axis in ["size", "angle"]
         assert ((self.anglerotBal+self.anglerange)<=90 and self.anglerotBal<=90) or ((self.anglerotBal+self.anglerange)>=90 and self.anglerotBal>=90)
         assert self.anglerotBal < 180
@@ -92,7 +104,7 @@ class Task:
         anglebounds = self.bounds['angle']
         return (angle_val*(anglebounds[1] - anglebounds[0])) + anglebounds[0]
     
-    def transformstim(self, stim):
+    def transform_stim(self, stim):
         ret = np.array(stim)
         stimflip = stim
         if self.swapcorners:
@@ -117,6 +129,11 @@ class Task:
         """
         rects = gen_rects()
         
+        regularlayout = [2, 3, 4, 3, 2,
+                         2, 3, 4, 3, 2]
+        stackedlayout = [5, 3, 4, 3, 2,
+                         2, 3, 4, 3, 5]
+        
         if self.stacked:
             layout = [5, 3, 4, 3, 2,
                       2, 3, 4, 3, 5]
@@ -124,21 +141,25 @@ class Task:
             layout = [2, 3, 4, 3, 2,
                       2, 3, 4, 3, 2]
         
-        reps = self.nlab / 28  # number of times the layout is repeated.
+        reps = self.nbasis / sum(regularlayout)  # number of times the layout is repeated.
         
         testreps = sum( sum(layout) * (np.arange(1, reps)) < self.ntest ) + 1
         trainreps = reps
         
         teststims = []
         trainstims = []
+        # DISCUSS: should we make all the rectangles squares?
+        # There are 10 
         for i in range(len(layout)):
             ntrain = layout[i] * trainreps
             ntest = layout[i] * testreps
-            traincoords = random_coords_in_rect( rects[i], n )
-            testcoords = random_coords_in_rect( rects[i], n )
+            traincoords = random_coords_in_rect( rects[i], ntrain )
+            testcoords = random_coords_in_rect( rects[i], ntest )
+            
+            # Most TVs are unlabeled, so we start with those and then add the others
             trainlabels = [np.nan for _ in xrange(ntrain)]
             testlabels = [np.nan for _ in xrange(ntest)]
-            # Add in labels if we're in a labeled area
+            # Add in labels to training if we're in a labeled area
             if i == 0:
                 assert (self.nlab/2) <= len(trainlabels), "Too many labels for the labeled box!"
                 trainlabels[:(self.nlab/2)] = [ 0 for _ in xrange( self.nlab/2 ) ]
@@ -149,90 +170,29 @@ class Task:
                 trainstims.append( list(traincoords[j]) + [trainlabels[j], i] )
             for j in xrange(len(testcoords)):
                 teststims.append( list(testcoords[j]) + [testlabels[j], i] )
-
-
-
-    # Make sure last 10 are unlabeled and evenly chosen from the two sides.
-    nprand.shuffle(stims)
-    unlab1 = [ stim for stim in stims if np.isnan( stim[2] ) and stim[3]<5 ]
-    unlab2 = [ stim for stim in stims if np.isnan( stim[2] ) and stim[3]>=5 ]
-    labeled = [ stim for stim in stims if not np.isnan( stim[2] ) ]
-    if not ntest:
-        lastten = unlab1[:5] + unlab2[:5]
-        rest = unlab1[5:] + unlab2[5:] + labeled
-        nprand.shuffle( rest )
-        nprand.shuffle( lastten )
         
-        return rest + lastten
-    else:
+        nprand.shuffle( trainstims )
+        nprand.shuffle( teststims )
+        
+        # Removed the last-ten thing.
+        # Make sure last 10 training items are unlabeled items drawn evenly
+        # from both groups
+        #unlab1 = [ stim for stim in trainstims if np.isnan( stim[2] ) and stim[3]<5 ]
+        #unlab2 = [ stim for stim in trainstims if np.isnan( stim[2] ) and stim[3]>=5 ]
+        #labeled = [ stim for stim in trainstims if not np.isnan( stim[2] ) ]
+        #lastten = unlab1[:5] + unlab2[:5]
+        #rest = unlab1[5:] + unlab2[5:] + labeled
+        #nprand.shuffle( rest )
+        #nprand.shuffle( lastten )
+        #trainstims = rest + lastten
+        
+        unlab1 = [ stim for stim in teststims if np.isnan( stim[2] ) and stim[3]<5 ]
+        unlab2 = [ stim for stim in teststims if np.isnan( stim[2] ) and stim[3]>=5 ]
         nprand.shuffle( unlab1 )
         nprand.shuffle( unlab2 )
-        assert ntest % 2 == 0
-        ret = unlab1[:ntest/2] + unlab2[:ntest/2]
-        nprand.shuffle( ret )
-        return ret
+        assert self.ntest % 2 == 0, "Number of test items must be even"
+        teststims = unlab1[:self.ntest/2] + unlab2[:self.ntest/2]
+        nprand.shuffle( teststims )
+        return trainstims, teststims
 
-def generate_stims( task, axisBal='size', anglerotBal=0, cornerBal=0, identity=0 ):
-    assert (axisBal in ['size', 'angle']), "Invalid axisbal: %s" % axisBal
-    axisBal = axisBal == 'angle'
-    print axisBal
-    sizerange  = 120
-    sizeoffset = 30
-    angleoffset = anglerotBal
-    anglerange = 60
-    bounds = [[ sizeoffset, sizerange+sizeoffset ], [angleoffset, angleoffset+anglerange]]
-    
-    def transform_size(size_val):
-        thesebounds = bounds[0]
-        return (size_val*(thesebounds[1] - thesebounds[0])) + thesebounds[0]
-    # y is angle
-    def transform_angle(angle_val):
-        thesebounds = bounds[1]
-        return (angle_val*(thesebounds[1] - thesebounds[0])) + thesebounds[0]
-    def transformstim( stim ):
-        ret = np.array(stim)
-        stimflip = stim
-        if cornerBal:
-            stimflip[1] = 1 - stim[1]
-        if axisBal:
-            ret[0] = transform_size( stimflip[1] )
-            ret[1] = transform_angle( stimflip[0] )
-        else:
-            ret[0] = transform_size( stimflip[0] )
-            ret[1] = transform_angle( stimflip[1] )
-        if not identity:
-            ret[2] = 1-stim[2]
-        return ret
-    
-    return [ transformstim(item) for item in task ]
-
-def build_train():
-    abstract_train = np.array(build_abstract_task(stacked, nlab, nbasis, ntrain))
-    trainstims = np.array(generate_stims(abstract_train, axisBal, anglerotBal,
-                                    cornerBal, identity))
-
-
-def build_block(order = "interspersed",
-                nlab=0,
-                ntrain=240,
-                ntest=0,
-                testform="bimod",
-                axis="size",
-                anglerotBal=0, 
-                cornerBal=0, 
-                identity=0,
-                anglerange = 60,
-                lengthrange = 120):
-    assert nlab % 2 == 0, "nlab must be even!"
-    
-    nbasis = 280;
-    if nlab > (nbasis / 7): # WARNING: Assumes traditional task structure.
-        stacked = True
-    
-    trainstims = trainstims[:,:-1] # Pinch off the rectangle identifier here (it's already accounted for).
-    
-    return map(list, np.hstack([abstract_task, stims]))
-
-class tvexp:
-    pass
 
