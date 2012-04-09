d742c75bf18078974a33451bceb5c753381ec40f
John McDonnell <john.v.mcdonnell@gmail.com>
2012-04-09 18:13:32 -0400

    Fixed pylab task plotter.

--- plottask.py
+++ plottask.py
@@ -5,52 +5,35 @@ import numpy as np
 import task as T
 
 def plot_task(task):
-    task = np.array( task )
-    labels = list(set( map(str, task[:,2]) ))
+    abstract = np.array( [x[1:4] for x in task] )
+    actual = np.array( [x[5:7] + [x[3]] for x in task] )
+    labels = list(set( map(str, [x[3] for x in task] ) ) )
     labels.sort()
     pylab.hold( True )
-    pylab.plot( task[:,0], task[:,1], "k" )
+    pylab.plot( actual[:,0], actual[:,1], "k" )
     cols = "rgw"
     for i in range(len(labels)):
-        coords = np.array([st[:2] for st in task if str(st[2]) == labels[i]])
+        coords = np.array([st[:2] for st in actual if str(st[2]) == labels[i]])
         pylab.plot( coords[:,0], coords[:,1], "o"+cols[i] )
 
 if __name__ == "__main__":
-    trainargs = {"stacked": False, 
-                 "ngrid": 0, 
-                 "nlab": 40,
-                 "ntest": 0, 
-                 "axisBal": "size",
-                 "anglerotBal": 101, 
-                 "cornerBal": 1,
-                 "identity": 1 }
-    pylab.subplot( 131 )
-    task = T.build_task( ** trainargs )
+    taskparams = T.condition_builder(0, 1)
+    taskobject = T.tvTask( ** taskparams )
+    
+    pylab.subplot( 121 )
+    train = taskobject.train
     print "First 3 trials:"
-    print task[:3]
-    plot_task(task[:,4:])
+    print train[:3]
+    plot_task(train)
     
     # Test phase
     print "Test phase"
-    pylab.subplot( 132 )
-    testargs = trainargs
-    testargs["nlab"] = 0
-    testargs["ntest"] = 50
-    task = T.build_task( **testargs )
+    pylab.subplot( 122 )
+    test = taskobject.test
     print "First 3 trials:"
-    print task[:3]
-    plot_task(task[:,4:])
+    print test[:3]
+    plot_task(test)
     
-    # Preview examples
-    print "Preview examples"
-    pylab.subplot( 133 )
-    demoargs = trainargs
-    demoargs["nlab"] = 0
-    demoargs["ngrid"] = 80
-    task = T.build_task( **demoargs )
-    print "First 3 trials:"
-    print task[:3]
-    plot_task(task[:,4:])
     pylab.show()
 
 
